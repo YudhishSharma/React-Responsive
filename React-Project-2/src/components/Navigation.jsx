029d3896c27985c483111877eaa7e6df59f8db4d
CodeWithYudi <djyudi23sep@gmail.com>
2023-09-28 14:03:10 +0530

    React Basic Projects

---
+++ React-Project-2/src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+const Navigation = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/Frame 2 1.png" alt="" />
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
